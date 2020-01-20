06efb5f7d329651809260dc0ce5eda91245c37fc
sanwailwin <sanwailwin21@gmail.com>
2020-01-20 13:22:50 +0630

    Create CRUD with Java

---
+++ src/test/java/com/san/springbootreact/SpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
